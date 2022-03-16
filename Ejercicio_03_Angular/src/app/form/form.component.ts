760e52d7635ff0869cc7e2853bd862c700ee98e2
Jorgee_sk <jgaliano2000@gmail.com>
2022-03-16 13:21:59 +0100

    Atributos formulario

--- Ejercicio_03_Angular/src/app/form/form.component.ts
+++ Ejercicio_03_Angular/src/app/form/form.component.ts
@@ -6,8 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  private _contact:string;
+  private _name:string;
+  private _email:string;
+  private _message:string;
 
-  constructor() { }
+  constructor() { 
+    this._contact = "Contáctame";
+    this._name = "Nombre completo";
+    this._email = "Tu email";
+    this._message = "Mensaje";
+  }
+
+  public get contact(){
+    return this._contact;
+  }
+
+  public set contact(value:string){
+    this._contact = value;
+  }
+
+  public get name(){
+    return this._name;
+  }
+
+  public set name(value:string){
+    this._name = value;
+  }
+
+  public get email(){
+    return this._email;
+  }
+
+  public set email(value:string){
+    this._email = value;
+  }
+
+  public get message(){
+    return this._message;
+  }
+
+  public set message(value:string){
+    this._message = value;
+  }
+
+
 
   ngOnInit(): void {
   }
