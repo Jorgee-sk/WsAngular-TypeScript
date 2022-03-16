869c3605a4e077815ce49630c31e23a71a159e9c
Jorgee_sk <jgaliano2000@gmail.com>
2022-03-16 12:18:28 +0100

    Insertado componente footer en Angular y quitado margin

---
+++ Ejercicio_03_Angular/src/app/profile-section/profile-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileSectionComponent } from './profile-section.component';
+
+describe('ProfileSectionComponent', () => {
+  let component: ProfileSectionComponent;
+  let fixture: ComponentFixture<ProfileSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfileSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Ejercicio_03_Angular/src/app/profile-section/profile-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-section',
+  templateUrl: './profile-section.component.html',
+  styleUrls: ['./profile-section.component.css']
+})
+export class ProfileSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
