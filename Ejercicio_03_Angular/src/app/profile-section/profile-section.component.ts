46eee2630785da9754dbeba4c9fb558630da9071
Jorgee_sk <jgaliano2000@gmail.com>
2022-03-16 13:40:25 +0100

    Atributos section profile y project

--- Ejercicio_03_Angular/src/app/profile-section/profile-section.component.ts
+++ Ejercicio_03_Angular/src/app/profile-section/profile-section.component.ts
@@ -6,8 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-section.component.css']
 })
 export class ProfileSectionComponent implements OnInit {
+  
+  private _name:string;
+  private _profesion:string;
+  private _grado:string;
 
-  constructor() { }
+  constructor() { 
+    this._name = "Jorge Galiano García"
+    this._profesion = "Estudiante"
+    this._grado = "diseño y desarrollo de videojuegos"
+  }
+
+  public get name(){
+    return this._name;
+  }
+
+  public set name(value:string){
+    this._name = value;
+  }
+
+  public get profesion(){
+    return this._profesion;
+  }
+
+  public set profesion(value:string){
+    this._profesion = value;
+  }
+
+  public get grado(){
+    return this._grado;
+  }
+
+  public set grado(value:string){
+    this._grado = value;
+  }
 
   ngOnInit(): void {
   }
--- Ejercicio_03_Angular/src/app/projects-section/projects-section.component.ts
+++ Ejercicio_03_Angular/src/app/projects-section/projects-section.component.ts
@@ -6,9 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-section.component.css']
 })
 export class ProjectsSectionComponent implements OnInit {
+  
+  private _p1:string;
+  private _p2:string;
+  private _p3:string;
+  
+  constructor() { 
+    this._p1 = "Frozen fury"
+    this._p2 = "Last nightfall"
+    this._p3 = "Aplicaciones de móvil"
 
-  constructor() { }
+  }
 
+  public get proyecto1(){
+    return this._p1;
+  }
+  public set proyecto1(value:string){
+    this._p1 = value;
+  }
+
+  public get proyecto2(){
+    return this._p2;
+  }
+  public set proyecto2(value:string){
+    this._p2 = value;
+  }
+  public get proyecto3(){
+    return this._p3;
+  }
+  public set proyecto3(value:string){
+    this._p3 = value;
+  }
   ngOnInit(): void {
   }
 
